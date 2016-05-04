b678e35434051779391247743f155f730950a9b4
Checkwhei Sin <Checkwhei Sin>
2016-05-04 18:59:27 -0400

    Multi Var finished

--- Coursera/homework/ex1/ex1_multi.m
+++ Coursera/homework/ex1/ex1_multi.m
@@ -82,16 +82,16 @@ X = [ones(m, 1) X];
 fprintf('Running gradient descent ...\n');
 
 % Choose some alpha value
-alpha = 0.01;
+alpha = 0.1;
 num_iters = 400;
 
 % Init Theta and Run Gradient Descent 
 theta = zeros(3, 1);
 [theta, J_history] = gradientDescentMulti(X, y, theta, alpha, num_iters);
-
 % Plot the convergence graph
 figure;
 plot(1:numel(J_history), J_history, '-b', 'LineWidth', 2);
+
 xlabel('Number of iterations');
 ylabel('Cost J');
 
@@ -104,7 +104,11 @@ fprintf('\n');
 % ====================== YOUR CODE HERE ======================
 % Recall that the first column of X is all-ones. Thus, it does
 % not need to be normalized.
-price = 0; % You should change this
+testX = [1, 1650, 3]; % this needs to be normalized
+
+testXNorm = testX;
+testXNorm(1, 2:3) = (testXNorm(1, 2:3) .- mu) ./ sigma;
+price = testXNorm * theta; % You should change this
 
 
 % ============================================================
@@ -149,7 +153,7 @@ fprintf('\n');
 
 % Estimate the price of a 1650 sq-ft, 3 br house
 % ====================== YOUR CODE HERE ======================
-price = 0; % You should change this
+price = testX * theta; % You should change this
 
 
 % ============================================================
