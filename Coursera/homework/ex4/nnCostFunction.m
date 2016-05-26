f20d94860f37a5f869796607c176b01490d15101
jackalsin <jackal.sin@hotmail.com>
2016-05-26 18:40:17 -0400

    hw4 finished

--- Coursera/homework/ex4/nnCostFunction.m
+++ Coursera/homework/ex4/nnCostFunction.m
@@ -54,13 +54,6 @@ Theta2_grad = zeros(size(Theta2));
 %               over the training examples if you are implementing it for the 
 %               first time.
 %
-% Part 3: Implement regularization with the cost function and gradients.
-%
-%         Hint: You can implement this around the code for
-%               backpropagation. That is, you can compute the gradients for
-%               the regularization separately and then add them to Theta1_grad
-%               and Theta2_grad from Part 2.
-%
 
 
 % deal with y
@@ -87,16 +80,30 @@ J = 1 / m * (sum(sum(logisf))) + lambda / (2*m) * (sum(sum(newTheta1 .^2)) + sum
 
 delta3 = a3 - y; % 5000 * 26
 delta2 = delta3 * Theta2 .* (a2 .* (1-a2));
+delta2 = delta2(:, 2: end);
 
 triDelta2 = zeros(num_labels, hidden_layer_size + 1);
 triDelta2 += delta3' * a2; 
 
-triDelta1 = zeros(hidden_layer_size + 1, size(X, 2));
+triDelta1 = zeros(hidden_layer_size, size(X, 2));
 triDelta1 += delta2' * a1;
 
 Theta1_grad = 1/m * triDelta1;
 
 Theta2_grad = 1/m * triDelta2;
+
+% Part 3: Implement regularization with the cost function and gradients.
+%
+%         Hint: You can implement this around the code for
+%               backpropagation. That is, you can compute the gradients for
+%               the regularization separately and then add them to Theta1_grad
+%               and Theta2_grad from Part 2.
+%
+
+Theta1_grad(:,2:end) += lambda/m .* Theta1(:, 2:end); 
+
+Theta2_grad(:,2:end) += lambda/m .* Theta2(:, 2:end);
+
 
 % -------------------------------------------------------------
 
